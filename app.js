b1b3826b0b71aee3767a16f0d7e172f5d2324c75
Tyler Smith <tylersmith.me@gmail.com>
2016-09-13 17:50:10 -0700

    FEATURE: Add state watching.

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const serverFactory = require("./lib/server");
 const port = process.env.OB_RELAY_PORT || 8080;
 const sessionSecret = process.env.OB_RELAY_SESSION_SECRET;
 const sessionDBFile = process.env.OB_RELAY_DB_FILE || "/home/openbazaar/obdata/OB-Mainnet.db";
+const stateFile = process.env.OB_RELAY_STATE_FILE || "/home/openbazaar/.easybazaar_state";
 
 // Create server
-serverFactory(port, sessionSecret, sessionDBFile).bind();
+serverFactory(port, sessionSecret, sessionDBFile, stateFile).bind();
 
 console.log("ob-relay listening on http://localhost:" + port);
