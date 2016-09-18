2c32f2fd8738ad3a97798235bbbe882b1d1a0f04
Tyler Smith <tylersmith.me@gmail.com>
2016-09-17 20:19:35 -0700

    TWEAK: Rename env vars to use *File convention.

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const serverFactory = require("./lib/server");
 const port = process.env.OB_RELAY_PORT || 8080;
 const sessionDBFile = process.env.OB_RELAY_DB_FILE || "/home/openbazaar/obdata/OB-Mainnet.db";
 const stateFile = process.env.OB_RELAY_STATE_FILE || "/home/openbazaar/.easybazaar_state";
-const sslCertFile = process.env.OB_RELAY_SSL_CERT;
-const sslKeyFile = process.env.OB_RELAY_SSL_KEY;
+const sslCertFile = process.env.OB_RELAY_SSL_CERT_FILE;
+const sslKeyFile = process.env.OB_RELAY_SSL_KEY_FILE;
 
 // Create server
 var server = serverFactory(sessionDBFile, stateFile);
